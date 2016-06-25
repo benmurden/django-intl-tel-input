df9c320737326eb2065e33b8919d447f464f2f78
Benjamin Murden <ben.murden@gmail.com>
2016-06-25 15:53:45 +0900

    Add script initialization and utils.

--- intl-tel-input/widgets.py
+++ intl-tel-input/widgets.py
@@ -1,3 +1,4 @@
+import json
 from django import forms
 
 
@@ -8,8 +9,20 @@ class IntlTelInputWidget(forms.TextInput):
         }
         js = ('https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/9.0.1/js/intlTelInput.min.js',)
 
-    def __init__(self, attrs=None, format=None):
+    def __init__(self, attrs=None, use_utils=True):
         final_attrs = {'class': 'phone-field', 'type': 'tel', 'size': '20'}
         if attrs is not None:
             final_attrs.update(attrs)
+
+        self.options = {}
+        if use_utils:
+            self.options['utilsScript'] = 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/9.0.1/js/utils.js'
+
         super(IntlTelInputWidget, self).__init__(attrs=final_attrs, format=format)
+
+    def render(self, name, value, attrs=None):
+        output_html = super(IntlTelInputWidget, self).render(name, value, attrs=attrs)
+        output_html += format_html('<script>$("#{}").intlTelInput({});</script>', self._id, self.get_options())
+
+    def get_options(self):
+        return json.dumps(self.options)
