81617b6a19bb71adbeb56791ffd3127becd9c793
Benjamin Murden <ben.murden@gmail.com>
2016-06-29 16:15:56 +0900

    Add geoIPLookup

--- intl_tel_input/static/intl_tel_input/init.js
+++ intl_tel_input/static/intl_tel_input/init.js
@@ -4,7 +4,15 @@
 
   inputs.each(function(i, el) {
     $el = $(el);
-    options = {};
+    options = {
+      initialCountry: "auto",
+      geoIpLookup: function(callback) {
+        $.get('http://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+          var countryCode = (resp && resp.country) ? resp.country : "";
+          callback(countryCode);
+        });
+      }
+    };
 
     if ($el.attr('data-utils-script')) {
       options['utilsScript'] = 'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/9.0.1/js/utils.js';
