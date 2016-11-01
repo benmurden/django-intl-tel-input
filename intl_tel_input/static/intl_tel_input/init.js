1a4c66da1c405e90b5bb4da03582ccc1b4fcd0fe
Benjamin Murden <ben.murden@gmail.com>
2016-11-01 15:05:25 +0900

    Better input handling and geo ip option.

--- intl_tel_input/static/intl_tel_input/init.js
+++ intl_tel_input/static/intl_tel_input/init.js
@@ -1,23 +1,26 @@
 (function($) {
-  var $el, options, $form, data, defaultCode,
+  var $el, $realInput, options, $form, data, defaultCode,
       cssClass = '.intl-tel-input',
       forms = [],
       inputs = $(cssClass);
 
   inputs.each(function(i, el) {
     $el = $(el);
+    $realInput = $el.prev();
     data = $el.data();
     defaultCode = data.defaultCode !== undefined ? data.defaultCode : 'us';
     options = {
-      initialCountry: "auto",
+      initialCountry: data.autoGeoIp ? 'auto' : data.defaultCode,
       geoIpLookup: function(callback) {
-        $.get('//freegeoip.net/json/', function() {}, "jsonp").done(function(resp) {
-          var countryCode = (resp && resp.country_code) ? resp.country_code : "";
-          callback(countryCode);
-        }).fail(function(jqXHR) {
-          console.warn('GeoIP Error: ' + jqXHR.status);
-          callback(defaultCode);
-        });
+        if (data.autoGeoIp) {
+          $.get('//freegeoip.net/json/', function() {}, "jsonp").done(function(resp) {
+            var countryCode = (resp && resp.country_code) ? resp.country_code : "";
+            callback(countryCode);
+          }).fail(function(jqXHR) {
+            console.warn('GeoIP Error: ' + jqXHR.status);
+            callback(defaultCode);
+          });
+        }
       },
       allowDropdown: data.allowDropdown !== undefined ? true : false
     };
@@ -29,12 +32,16 @@
       $form = $el.closest('form');
       if (forms.indexOf($form) === -1) {
         $form.submit(function(e) {
-          $form.find('input' + cssClass).val(function() {
-            return $(this).intlTelInput("getNumber");
+          $realInput.val(function() {
+            return $el.intlTelInput("getNumber");
           });
         });
 
         forms.push($form);
+      }
+
+      if ($realInput.val() !== '') {
+        $el.intlTelInput('setNumber', $realInput.val());
       }
     });
   });
--- intl_tel_input/widgets.py
+++ intl_tel_input/widgets.py
@@ -1,5 +1,9 @@
 import json
 from django import forms
+from django.forms.utils import flatatt
+from django.utils.encoding import force_text
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 
 class IntlTelInputWidget(forms.TextInput):
@@ -11,16 +15,40 @@ class IntlTelInputWidget(forms.TextInput):
         }
         js = ('https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/9.0.1/js/intlTelInput.min.js', 'intl_tel_input/init.js')
 
-    def __init__(self, attrs=None, allow_dropdown=True, preferred_countries=['us', 'gb'], default_code='us'):
-        final_attrs = {'class': 'intl-tel-input', 'size': '20'}
+    def __init__(self, attrs=None, allow_dropdown=True, preferred_countries=['us', 'gb'], default_code='us', auto_geo_ip=False):
+        final_attrs = {'size': '2'}
         if attrs is not None:
             final_attrs.update(attrs)
 
-        final_attrs['data-allow-dropdown'] = allow_dropdown
-        final_attrs['data-preferred-countries'] = json.dumps(preferred_countries)
-        final_attrs['data-default-code'] = default_code
+        self.js_attrs = {
+            'size': '20',
+            'data-allow-dropdown': allow_dropdown,
+            'data-preferred-countries': json.dumps(preferred_countries),
+            'data-default-code': default_code,
+            'data-auto-geo-ip': auto_geo_ip,
+        }
 
         super(IntlTelInputWidget, self).__init__(attrs=final_attrs)
 
     def get_options(self):
         return json.dumps(self.options)
+
+    def render(self, name, value, attrs=None):
+        if value is None:
+            value = ''
+
+        final_attrs = self.build_attrs(attrs, name=name, size=2)
+        final_attrs['type'] = 'hidden'
+        if value != '':
+            final_attrs['value'] = force_text(self._format_value(value))
+
+        self.js_attrs['class'] = ' '.join(['intl-tel-input', final_attrs.get('class', '')]).strip()
+
+        output = [format_html('<input{}>', flatatt(final_attrs))]
+        select = self.render_select()
+        output.append(select)
+        return mark_safe('\n'.join(output))
+
+    def render_select(self):
+        output = format_html('<input{}>', flatatt(self.js_attrs))
+        return output
